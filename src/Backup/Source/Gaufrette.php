6505ca3e63ff4e614714e32edbe75e64c68c9c2b
Adam Brett <adam@adambrett.co.uk>
2013-01-10 16:18:11 +0000

    WIP

---
+++ src/Backup/Source/Gaufrette.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Backup
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @category Backup
+ * @package  Backup
+ * @author   Adam Brett <adam@adambrett.co.uk>
+ * @license  New BSD LICENSE
+ * @version  0.1
+ * @link     http://github.com/adambrett/php-backup
+ */
+
+namespace Backup\Source;
+
+use Backup\Exception;
+
+/**
+ * Backup Gaufrette Source
+ *
+ * @category Backup
+ * @package  Backup
+ * @author   Adam Brett <adam@adambrett.co.uk>
+ * @license  New BSD LICENSE
+ * @link     https://github.com/adambrett/php-backup
+ */
+class Gaufrette implements SourceInterface
+{
+    protected $adapter;
+
+    public function __construct(\Gaufrette\Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * Get File
+     *
+     * @param string $filename name / path to file to get
+     *
+     * @return string contents of the file
+     */
+    public function getFileContents($filename)
+    {
+        $this->adapter->read($filename);
+    }
+
+    /**
+     * DirectoryList
+     *
+     * @param string $directory name / path of directory to list contents for
+     *
+     * @return array of file and directory paths
+     */
+    public function directoryList($directory)
+    {
+        return $this->adapter->keys($directory);
+    }
+
+    /**
+     * IsDirectory
+     *
+     * Test to see if a path is a directory or a file
+     *
+     * @param mixed $path path to test.
+     *
+     * @return bool
+     */
+    public function isDirectory($path)
+    {
+        return $this->adapter->isDirectory($path);
+    }
+}
